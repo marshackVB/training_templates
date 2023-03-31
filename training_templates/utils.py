410b6e54154a3837c2e19a2c8a16730f4177b891
marshackVB <marshackVB>
2023-03-31 00:35:42 +0000

    remove yaml import

--- training_templates/utils.py
+++ training_templates/utils.py
@@ -1,7 +1,6 @@
 from argparse import Namespace
 
 import mlflow
-import yaml
 
 
 def get_or_create_experiment(experiment_location: str) -> None:
@@ -16,29 +15,6 @@ def get_or_create_experiment(experiment_location: str) -> None:
         mlflow.create_experiment(experiment_location)
 
     mlflow.set_experiment(experiment_location)
-
-
-def get_yaml_config(config_file: str) -> Namespace:
-    """
-    Loads paramerts from a yaml configuration file in a
-    Namespace object.
-
-    Args:
-      config_file: A path to a yaml configuration file.
-
-    Returns:
-      A Namespace object that contans configurations referenced
-      in the program.
-    """
-    stream = open(config_file, "r")
-    config_dict = yaml.load(stream, yaml.SafeLoader)
-
-    for parameter, value in config_dict.items():
-        print("{0:30} {1}".format(parameter, value))
-
-    config = Namespace(**config_dict)
-
-    return config
 
 
 def get_commit_info(commit_sha, release_version):
