62bd794b383b31efe1f7261c5fee80964efd23ec
marshackVB <marshackVB>
2023-04-18 01:01:51 +0000

    change formatting logic

--- training_templates/tuners/formatters.py
+++ training_templates/tuners/formatters.py
@@ -1,34 +1,42 @@
 from typing import Callable, Dict, Union
 
-def format_hyperopt_sklearn(hyperopt_params: Dict[str, Union[str, int, float]], model: Callable) -> Dict[str, Union[str, int, float]]:
+
+def convert(hyperopt_params, convert_to_int_list):
+    for param, value in hyperopt_params.items():
+            if param in convert_to_int_list:
+                hyperopt_params[param] = int(value)
+
+    return hyperopt_params
+
+
+
+def format_hyperopt_sklearn(hyperopt_params: Dict[str, Union[str, int, float]]) -> Dict[str, Union[str, int, float]]:
     """
     Reformat hyperopt parameters for sklearn
     """
+    convert_to_int = [
+    "n_estimators",
+    "max_depth",
+    "max_leaf_nodes"
+    ]
 
-    for param, value in hyperopt_params.items():
-        defaul_value = model.__dict__[param]
-        if isinstance(defaul_value, int):
-            hyperopt_params[param] = int(hyperopt_params[param])
-    return hyperopt_params
+    converted = convert(hyperopt_params, convert_to_int)
+    return converted
 
 
 def format_hyperopt_xbgoost(hyperopt_params: Dict[str, Union[str, int, float]])-> Dict[str, Union[str, int, float]]:
     """
-    Reformat hyperopt parameters for sklearn
+    Reformat hyperopt parameters for xgboost
     """
-        
     convert_to_int = [
     "n_estimators",
     "max_depth",
     "max_leaves",
     "max_bin",
     "grow_policy",
-]
+    ]
         
-    for param, value in hyperopt_params.items():
-        if param in convert_to_int:
-            hyperopt_params[param] = int(value)
-
-    return hyperopt_params
+    converted = convert(hyperopt_params, convert_to_int)
+    return converted
 
 
